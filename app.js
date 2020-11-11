128b97e204e5cbc0e38c9f79ae328b9e7b04d224
Ashni-Nay Wilson <ashninaywilson@yahoo.com>
2020-11-10 21:25:12 -0500

    Added Javascript

---
+++ app.js
@@ -0,0 +1,26 @@
+/* Add JavaSxript here*/
+
+window.onload = function(){
+	let sbuttn = document.getElementById("searchbutt").addEventListener("click");
+
+    var httpRequest = new XMLHttpRequest();
+	var url = "http://localhost/info2180-lab4/superheroes.php";
+	httpRequest.onreadystatechange = superhero;
+	
+	function superhero(){	
+	if (httpRequest.readyState === XMLHttpRequest.DONE){
+		if (httpRequest.status === 200) {
+			var response = httpRequest.responseText;
+			var result = document.querySelector('result');
+			result.innerHTML = response;
+		}
+		else{
+			alert("There was a problem");
+		}
+	
+	
+	httpRequest.open("GET",url);
+	httpRequest.send();
+	}
+}
+});
